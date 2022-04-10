7ecde914cd346bcfdb3fc4d5187eaa7ed4abf732
Vinicius7199 <viniciusdeandradefernandes5@gmail.com>
2022-04-10 19:40:28 -0300

    Att comandos

--- comandos/comandos.js
+++ comandos/comandos.js
@@ -6,8 +6,12 @@ module.exports.run = (client, message, args) => {
     .setTitle('Comandos')
     .setThumbnail(`${client.user.avatarURL({ format: 'png' })}?size=1024`)
     .setDescription('Aqui estão meus comandos:')
-    .addField(`${process.env.PREFIX}cursos`, 'Lista de cursos')
     .addField(`${process.env.PREFIX}faq`, 'Perguntas frequentes')
+    .addField(`${process.env.PREFIX}blog`, 'Blog da Impacta')
+    .addField(`${process.env.PREFIX}cursos`, 'Cursos da Impacta')
+    .addField(`${process.env.PREFIX}contato`, 'Contato da Impacta')
+    .addField(`${process.env.PREFIX}graduacao`, 'Graduações da Impacta')
+    .addField(`${process.env.PREFIX}tecnicos`, 'Técnicos da Impacta')
     .setTimestamp();
 
     return message.reply({ embeds: [embed]})
